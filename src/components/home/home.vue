bc14d3c18c85fddfa66480e2b4cd5bc9e22487b9
Daniel Valenzuela <danielpipe2203@gmail.com>
2025-05-06 21:44:24 -0500

    Home - Hero sectionb basic styles

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,33 +1,34 @@
 <script setup>
-// import { ref } from 'vue';  habilitar solo para cuando se necesiten estados reactivos
 import SocialLinks from '@/components/SocialLinks/SocialLinks.vue';
 import ProjectsCarousel from '@/components/ProjectsCarousel/ProjectsCarousel.vue';
 import ProjectDetail from '@/components/ProjectDetail/ProjectDetail.vue';
 </script>
 <template>
     <!-- Hero -->
-    <section class="hero" id="hero-section">
+    <section class="hero__container" id="hero-section">
         <div class="row center-content">
             <div class="col-12 col-md-6">
-                <span>Hi, I’m</span>
-                <h2 class="main-title">
-                    <span class="name">DANIEL VALENZUELA</span>
-                    <hr>
-                    <span class="position">FRONT END DEVELOPER</span>
-                </h2>
+                <div class="name-txt__container">
+                    <span>Hi, I’m</span>
+                    <div class="name-txt__title">
+                        <h2 class="name">DANIEL VALENZUELA</h2>
+                        <hr>
+                        <h2 class="profession">FRONT END DEVELOPER</h2>
+                    </div>
+                </div>
                 <p class="description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat.
                 </p>
-                <div class="call-to-action center-content start-content">
-                    <button class="ui-btn">Projects</button>
-                    <button class="ui-btn dark-btn">Hire me 😎</button>
+                <div class="call-to-action center-content start-content button-group">
+                    <button class="ui-btn ui-btn--lg">Projects</button>
+                    <button class="ui-btn dark-btn ui-btn--lg">Hire me 😎</button>
                 </div>
                 <SocialLinks />
             </div>
-            <div class="col-12 col-md-6">
-                <img src="../../assets/profile.jpg" alt="Temp">
+            <div class="col-12 col-md-6 center-content hero-img">
+                <img src="../../assets/profile.jpg" alt="Temp" class="img-fluid">
             </div>
         </div>
     </section>
