207f3531a8276de3d72e2e7b1f5ef2d0fcebd147
Daniel Valenzuela <danielpipe2203@gmail.com>
2025-07-29 17:08:48 -0500

    Image and DB optimization pre commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/components/home/home.vue'
 import ProjectDetail from '@/components/ProjectDetail/ProjectDetail.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/project/:id', name: 'ProjectDetail', component: ProjectDetail, props: true, },
+  { path: '/project/:id', name: 'ProjectDetail', component: ProjectDetail, props: true },
+  { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  base: '/dvprod7/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/dvprod7/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src') // 👈 Define '@/' como alias de 'src/'
     }
   }
-})
+}))
