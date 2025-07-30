d525c4238a78fc7779727ac12820cb127b30be27
Daniel Valenzuela <danielpipe2203@gmail.com>
2025-07-29 21:04:04 -0500

    vite config bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/dvprod7/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   }
-}))
+})
